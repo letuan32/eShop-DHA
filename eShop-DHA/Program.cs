d7e4b4a62bcf2ac958c6baca1dd11deab6c43baf
letuan32 <letuanlttt@gmail.com>
2022-04-08 16:24:29 +0700

    config Swagger

--- eShop-DHA/Program.cs
+++ eShop-DHA/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment;
 // Add services to the container.
@@ -5,7 +8,26 @@ var env = builder.Environment;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Supermarket API",
+        Version = "v1",
+        Description = "Simple RESTful API built with ASP.NET Core 6",
+        Contact = new OpenApiContact
+        {
+            Name = "Tuan Le",
+            Url = new Uri("#")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT",
+        },
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 if (env.IsDevelopment())
 {
     
@@ -24,11 +46,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "eShop-DHA API V1");
+        c.RoutePrefix = string.Empty;
+        c.DocumentTitle = "eShop API";
+    });
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
