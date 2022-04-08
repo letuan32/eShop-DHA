1d8cde224ea50913304da01ed46ffd10887dee3d
letuan32 <letuanlttt@gmail.com>
2022-04-08 15:16:37 +0700

    enable listen port

--- eShop-DHA/Program.cs
+++ eShop-DHA/Program.cs
@@ -1,13 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var env = builder.Environment;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+if (env.IsDevelopment())
+{
+    
+}
+else
+{
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.ListenAnyIP(Int32.Parse(Environment.GetEnvironmentVariable("PORT")));
+    });
+}
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
